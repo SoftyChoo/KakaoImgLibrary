ff2705404237a068575032f9c5350d773b76bf01
추민수 <devsoftychoo@gmail.com>
2023-09-20 19:36:07 +0900

    [Feat] : 최신순서 개발 완료

--- app/src/main/java/com/example/kakaoimglibrary/search/SearchViewModel.kt
+++ app/src/main/java/com/example/kakaoimglibrary/search/SearchViewModel.kt
@@ -20,45 +20,10 @@ class SearchViewModel(
 
     fun searchImage(searchText : String){
         viewModelScope.launch {
-            val response = repository.searchImage(searchText,"accuracy")
+            val response = repository.searchImage(searchText,"recency")
             val body = response.body()
             _myPosts.value = body?.documents
             metadata = body?.metaData
         }
     }
 }
-
-
-//class SearchViewModel(private val idGenerate: AtomicLong) : ViewModel() {
-//
-//    private val _list: MutableLiveData<List<SearchModel>> = MutableLiveData()
-//    //읽기전용
-//    val list: LiveData<List<SearchModel>> get() = _list
-//
-//    init {
-//        _list.value = arrayListOf<SearchModel>().apply {
-//            for (i in 0 until 10) {
-//                add(
-//                    SearchModel(
-//                        idGenerate.getAndIncrement(),
-//                        title = "test $i",
-//                        datetime = "test:time: $i",
-//                        url = "https://softychoo.github.io/assets/img/me.JPG",
-//                        isBookmark = false
-//                    )
-//                )
-//            }
-//        }
-//    }
-//
-//    fun modifySearchModel(searchModel: SearchModel, position: Int?){
-//        val currentList = list.value.orEmpty().toMutableList()
-//        val findPosition = position ?: currentList.findIndex(searchModel)
-//        currentList[findPosition] = searchModel
-//        _list.value = currentList
-//    }
-//
-//    fun List<SearchModel>.findIndex(searchModel: SearchModel): Int {
-//        return indexOfFirst { it.id == searchModel.id } // indexOfFirst : 가장 먼저 찾는 값 | 없으면 -1 return
-//    }
-//}
